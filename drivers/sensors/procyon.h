7c30ec9f8fd6c7858fcff563646d3391417da7fa
George Norton <george_norton_uk@hotmail.com>
2026-05-18 11:24:38 +0100

    Tweak pressure coefficient.

--- drivers/sensors/procyon.h
+++ drivers/sensors/procyon.h
@@ -28,7 +28,9 @@
 #    define MXT_ACTIVE_ACQUISITION_INTERVAL 255 // Free run while active - this enables us to reach a polling rate of around 300Hz
 #    define MXT_CONFTHR 5 // Increase debounce time
 #    define MXT_MOVE_HYSTERESIS_NEXT 16
-#    define MXT_T100_AMPL_COEFF 11 // Scale pressure reports
+#    ifndef MAX_T100_AMPL_OFFSET
+#        define MXT_T100_AMPL_COEFF 16 // Scale pressure reports
+#    endif
 #endif
 
 // Board specific configuration
